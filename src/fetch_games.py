2175c2001da75febc73660a1a9e55e50860e5b9d
Sushant Shinde <98010962+Maverick341@users.noreply.github.com>
2025-04-08 00:39:04 +0530

    fix: ensure end date and URL are only processed for free games with active promotions and discountPercentage equal to Zero

--- src/fetch_games.py
+++ src/fetch_games.py
@@ -18,22 +18,23 @@ def get_free_games():
         promotions = game.get("promotions", {})
         if promotions and promotions.get("promotionalOffers"):
             offer = promotions["promotionalOffers"][0]["promotionalOffers"][0]
-            end_date = datetime.datetime.fromisoformat(offer["endDate"][:-1])  # Remove 'Z' from ISO format
+            if offer['discountSetting']['discountPercentage'] == 0:
+                end_date = datetime.datetime.fromisoformat(offer["endDate"][:-1])  # Remove 'Z' from ISO format
 
-            # Get the correct URL
-            product_slug = game.get("productSlug")
-            if not product_slug:  # If productSlug is missing, check for an alternative link
-                offer_mappings = game.get("offerMappings", [])
-                if offer_mappings:
-                    product_slug = offer_mappings[0].get("pageSlug")
+                # Get the correct URL
+                product_slug = game.get("productSlug")
+                if not product_slug:  # If productSlug is missing, check for an alternative link
+                    offer_mappings = game.get("offerMappings", [])
+                    if offer_mappings:
+                        product_slug = offer_mappings[0].get("pageSlug")
 
-            game_url = f'https://store.epicgames.com/en-US/p/{product_slug}' if product_slug else "https://store.epicgames.com/free-games"
+                game_url = f'https://store.epicgames.com/en-US/p/{product_slug}' if product_slug else "https://store.epicgames.com/free-games"
 
-            games.append({
-                "title": game["title"],
-                "url": game_url,
-                "end_date": end_date.strftime("%A, %B %d, %Y at %I:%M %p UTC")  # Formatted date
-            })
+                games.append({
+                    "title": game["title"],
+                    "url": game_url,
+                    "end_date": end_date.strftime("%A, %B %d, %Y at %I:%M %p UTC")  # Formatted date
+                })
 
     return games
 
